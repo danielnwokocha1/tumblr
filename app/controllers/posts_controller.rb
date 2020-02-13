7c3aaddc081395cbc95e4ff548f89bb2cd6d1c28
daniel :) <hgpmac151@Thes-MacBook-Air.local>
2020-02-12 17:34:55 -0800

    Add devise and users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @posts = Post.all.order('created_at DESC ')
+    @posts = Post.all.order('created_at DESC')
   end
 
   def new
